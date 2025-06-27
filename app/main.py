f84eae596e6d9681171b7d9875ea345d4812f0aa
ou786 <osamamohammed.ussaid@dentistryautomation.com>
2025-06-27 16:30:09 +0530

    Updated CORS Setup

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],  # ✅ React dev server
+    allow_origins=["https://meechat.vercel.app/"],  # ✅ React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
